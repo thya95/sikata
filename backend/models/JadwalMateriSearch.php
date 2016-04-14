4b97a3995dd67ede8ea0c1eb1f2cc690da7e24fe
intan <intanianz.amalia@gmail.com>
2016-04-14 21:21:38 +0700

    intan edit jadwal-materi

--- backend/models/JadwalMateriSearch.php
+++ backend/models/JadwalMateriSearch.php
@@ -6,7 +6,7 @@ use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use app\models\JadwalMateri;
-use yii\jui\DatePicker;
+
 
 /**
  * JadwalMateriSearch represents the model behind the search form about `app\models\JadwalMateri`.
